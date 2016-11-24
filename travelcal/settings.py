f76af72b7c1ef5bce3719653ddd1769617937bd6
Rick Ashford <rick.ashford@suse.com>
2016-11-24 00:19:43 -0600

    Data migration

--- travelcal/settings.py
+++ travelcal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ical.apps.IcalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
